261c71fae3960896c26c148ab9cb0bc933e6bf08
SHaider0 <Samnoon.Haider@gmail.com>
2019-11-26 03:11:18 -0500

    send message when user not found

--- handlers/potentialFriends.js
+++ handlers/potentialFriends.js
@@ -5,6 +5,9 @@ exports.getPotentialFriend = async function(req, res, next) {
     let user = await db.User.findOne({
       username: req.params.username
     });
+    if (!user) {
+      return res.status(200).json({message: "user not found"})
+    }
     return res.status(200).json(user);
   } catch (err) {
     return next(err);
