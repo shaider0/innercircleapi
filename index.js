f768711569ebe8fd666567246c28ec04314cd1f4
SHaider0 <Samnoon.Haider@gmail.com>
2019-12-03 08:49:35 +0100

    create endpoint for handling image upload to s3 bucket

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const AWS = require('aws-sdk')
+const fs = require('fs')
+const fileType = require('file-type')
+const bluebird = require('bluebird')
+const multiparty = require('multiparty')
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const errorHandler = require("./handlers/error");
@@ -14,6 +19,44 @@ const potentialFriendsRoutes = require("./routes/potentialFriends");
 const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
 const db = require("./models");
 const PORT = 8081;
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
+
+AWS.config.setPromisesDependency(bluebird)
+
+const s3 = new AWS.S3()
+
+const uploadFile = (buffer, name, type) => {
+  const params = {
+    ACL: 'public-read',
+    Body: buffer,
+    Bucket: process.env.S3_BUCKET,
+    ContentType: type.mime,
+    Key: `${name}.${type.ext}`
+  };
+  return s3.upload(params).promise();
+};
+
+app.post('/test-upload', (request, response) => {
+  const form = new multiparty.Form();
+    form.parse(request, async (error, fields, files) => {
+      if (error) throw new Error(error);
+      try {
+        const path = files.file[0].path;
+        const buffer = fs.readFileSync(path);
+        const type = fileType(buffer);
+        const timestamp = Date.now().toString();
+        const fileName = `bucketFolder/${timestamp}-lg`;
+        const data = await uploadFile(buffer, fileName, type);
+        return response.status(200).send(data);
+      } catch (error) {
+        return response.status(400).send(error);
+      }
+    });
+});
 
 app.use(cors());
 
