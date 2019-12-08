864443b0dab9fd2f17b0737b5af858079536976b
SHaider0 <Samnoon.Haider@gmail.com>
2019-12-08 10:32:51 -0600

    minor update

--- handlers/messages.js
+++ handlers/messages.js
@@ -7,18 +7,12 @@ exports.sendMessage = async function(req, res, next) {
       username: req.body.recipientUsername
     })
 
-    console.log(req.params.id)
-    console.log(recipient[0]._id)
-    console.log(req.body.message)
-
     let message = await db.Message.create({
       sender: req.params.id,
       recipient: recipient[0]._id,
       message: req.body.message,
       user: req.params.id
     });
-
-    console.log("### function is hitting", message)
 
     return res.status(200).json(message);
 
@@ -36,7 +30,6 @@ exports.getMessages = async function(req, res, next) {
       username: true,
       profileImageUrl: true
     })
-    console.log('XXX', messages)
     return res.status(200).json(messages)
   } catch (err) {
     return next(err)
---
+++ routes/conversations.js
@@ -0,0 +1,20 @@
+
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+
+const {
+  createFriendRequest,
+  deleteFriendRequest,
+  getFriendRequests,
+  updateFriendRequest
+} = require("../handlers/friendRequests");
+
+router.route("/").post(createFriendRequest);
+router.route("/").get(getFriendRequests);
+
+router
+  .route("/:friendRequest_id")
+    .patch(updateFriendRequest)
+    .delete(deleteFriendRequest)
+
+module.exports = router;
